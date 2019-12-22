ebd12fd8810c448ad9fb0ac555a0caabc18fb76a
Muhammad Afif Alaptagin <afiffifa89@gmail.com>
2019-12-22 09:01:26 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,14 +1,12 @@
-array = ["fiddleheads","okra","kohlrabi"]
+array = ["apple", "grape"]
 def oxford_comma(array)
-  if array.length == 2 
-    array[-2] << " and "
-    array.join
-    elsif array.length == 1 
-    array.join
-    elsif array.length > 2 
-    array[-1].prepend " and "
-    array.join(", ")
+  if array.length == 2
+  array[-2] << " and "
+  array.join
+  elsif array.length == 1
+  	array.join
+  elsif array.length > 2
+    array[-1].prepend "and "
+	array.join(", ")
   end
-    
-    
 end
